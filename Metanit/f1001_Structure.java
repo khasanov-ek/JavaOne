ef158bd9df4f740a88c392795278dcdddbf8e557
Khasanov Eduard <mr_eduard@inbox.ru>
2023-02-02 20:32:14 +0500

    1

---
+++ Metanit/f1001_Structure.java
@@ -0,0 +1,24 @@
+/* 
+https://metanit.com/java/tutorial/2.11.php
+Основы программирования 
+  Структура программы
+	
+  public - модификатор доступа	
+  class  - название класса 
+  main	 - метод входная точка
+  static - статический метод 
+  void 	 - не возвращает никакого значения
+  String[] args - параметры метода (массив через которые можем передать в 
+									программу  различнвые  данные)
+
+*/
+
+
+public class f1001_Structure {
+	
+	public static void main(String[] args) {
+		System.out.println("F1001 Structure");
+		System.out.println("Hello Java!");
+	}
+	
+}
