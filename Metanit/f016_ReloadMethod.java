77e211a4e3b637f299250c5d5817b2f6921d4149
Khasanov Eduard <mr_eduard@inbox.ru>
2023-03-30 19:20:45 +0500

    1

--- Metanit/f016_ReloadMethod.java
+++ Metanit/f016_ReloadMethod.java
@@ -2,6 +2,29 @@ public class f016_ReloadMethod {
 	
 	public static void main(String[] args) {
 		System.out.println("Перегрузка методов");
+		/*
+		 В программе мы можем использовать методы с одним и тем же именем, но с разными типами и/или количеством параметров. 
+		 Такой механизм называется перегрузкой методов (method overloading).
+		*/
+		
+		System.out.println("sum(1) = " + sum(2, 3));
+		System.out.println("sum(2) = " + sum(4.5, 3.2));
+		System.out.println("sum(3) = " + sum(4, 3, 7));
+		
 	}
+	
+	
+	static int sum(int x, int y) {
+		return x + y;
+	}
+	
+	static double sum(double x, double y) {
+		return x + y;
+	}
+
+	static int sum(int x, int y, int z) {
+		return x + y + z;
+	}
+	
 	
 }
